df32542781ce249a49ab987aa058784f87f4b3f7
Asel <aselsnic@gmail.com>
2026-03-16 12:34:32 +0530

    Updated the firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARJQ79KHf8SNlGHWA6PDjjCims_EpKxho",
+  apiKey: "AIzaSyARJQ79KHf8SN1GHWA6PDjjCims_EpKxho",
   authDomain: "task-tracker-960df.firebaseapp.com",
   projectId: "task-tracker-960df",
   storageBucket: "task-tracker-960df.firebasestorage.app",
@@ -13,5 +13,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Auth and Firestore, and export them!
 export const auth = getAuth(app);
 export const db = getFirestore(app);
